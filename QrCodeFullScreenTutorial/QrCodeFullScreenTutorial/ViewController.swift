af1d1a6c8f38d08cfeef320cd03ba0bf07cdb883
YounByoungil <jimmy7525@naver.com>
2021-08-26 02:00:25 +0900

    Setup Project

---
+++ QrCodeFullScreenTutorial/QrCodeFullScreenTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QrCodeFullScreenTutorial
+//
+//  Created by 윤병일 on 2021/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
